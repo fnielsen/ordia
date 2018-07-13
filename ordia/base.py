10ba7906df3430c07433cac72b3ce5fe501725ed
fnielsen <fn@imm.dtu.dk>
2018-07-13 15:54:05 +0200

    Add documentation

--- ordia/base.py
+++ ordia/base.py
@@ -75,13 +75,33 @@ class Base(object):
 
     Attributes
     ----------
+    entities : Entities
+        Structure to save/cache entities from Wikidata.
     form_index : defaultdict of defaultdict of list
         Dictionary by language.
+    grammatical_feature_index : defaultdict of list
+        Dictionary of grammatical features.
+    keyword_index : defaultdict of list
+        Dictionary of the lemmas and forms.
+    language_index : defaultdict of list
+        Dictionary of languages
+    lexical_category_counts : defaultdict of int
+        Dictionary with counts.
 
     """
 
     def __init__(self, max_ids=None):
-        """Initialize attributes."""
+        """Initialize attributes.
+
+        Parameters
+        ----------
+        max_ids : int or None
+            Maximum identifier to set up. To load the entire lexical data or
+            Wikidata this should be set to None. For quick set up during
+            development and debugging `max_ids` can be set to, e.g., 200.
+            This variable is passed on to the setup method.
+
+        """
         self.entities = Entities()
         self.form_index = defaultdict(lambda: defaultdict(list))
         self.grammatical_feature_index = defaultdict(list)
@@ -93,7 +113,16 @@ class Base(object):
         self.build_indices()
 
     def initialize_entities_from_api(self, max_ids=None):
-        """Initialize entities attributes from Wikidata API."""
+        """Initialize entities attributes from Wikidata API.
+
+        Parameters
+        ----------
+        max_ids : int or None
+            Maximum identifier to set up. To load the entire lexical data or
+            Wikidata this should be set to None. For quick set up during
+            development and debugging `max_ids` can be set to, e.g., 200.
+
+        """
         if max_ids is None:
             max_ids = 20000
         ids = ['L{}'.format(id_) for id_ in range(1, max_ids + 1)]
@@ -114,9 +143,8 @@ class Base(object):
             # Forms
             for form in entity['forms']:
                 for grammatical_feature in form['grammaticalFeatures']:
-                    self.grammatical_feature_index[grammatical_feature] \
-                        = id_
-
+                    self.grammatical_feature_index[
+                        grammatical_feature].append(form['id'])
                 for representation in form['representations'].values():
                     self.form_index[representation['language']][
                         representation['value']].append(
@@ -138,6 +166,12 @@ class Base(object):
         -------
         lemmas : list of str
             List of strings.
+
+        Examples
+        --------
+        >>> base = Base(max_ids=20)
+        >>> 'describe' in base.id_to_lemmas('L10')
+        True
 
         """
         entity = self.entities.get(id_)
@@ -182,6 +216,12 @@ class Base(object):
         -------
         ids : list of str
             List of strings representing lexeme forms.
+
+        Examples
+        --------
+        >>> base = Base(max_ids=20)
+        >>> 'L10-F4' in base.word_to_form_ids('describe', 'en')
+        True
 
         """
         return self.form_index[language][word]
