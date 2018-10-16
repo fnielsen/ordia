5f421c9c7760755acc46f9c20786325860983a9f
fnielsen <fn@imm.dtu.dk>
2018-10-16 15:31:22 +0200

    Add template for commandline interface

---
+++ ordia/__main__.py
@@ -0,0 +1,15 @@
+"""
+Usage:
+  ordia
+
+Description:
+  Ordia is a Python package and a webservice for the lexical data in Wikidata.
+
+  The webservice can be started with `python app.py`.
+
+"""
+
+from docopt import docopt
+
+
+arguments = docopt(__doc__)
