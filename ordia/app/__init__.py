07b1fcedb0f25513518c4896d0a9fe46d06e7e25
fnielsen <fn@imm.dtu.dk>
2019-03-01 19:12:09 +0100

    Implement localization with Danish
    Work in progress. It is not clear how efficient the library is and
    getting translation is not straightforward.

--- ordia/app/__init__.py
+++ ordia/app/__init__.py
@@ -3,7 +3,18 @@
 
 from __future__ import absolute_import, division, print_function
 
+from os.path import join
+
 from flask import Flask
+from flask_jsonlocale import Locales
+
+
+class OrdiaLocales(Locales):
+    def get_message(self, message_code, language=None):
+        message = super(OrdiaLocales, self).get_message(message_code, language)
+        if message is None:
+            message = message_code
+        return message
 
 
 def create_app():
@@ -19,6 +30,9 @@ def create_app():
     """
     app = Flask(__name__)
 
+    app.config.setdefault("MESSAGES_DIR", join('ordia', 'app', 'messages'))
+    OrdiaLocales(app)
+    
     from .views import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
