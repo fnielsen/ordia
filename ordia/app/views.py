53bb996a191e27ade788d5510b6925977dcc988a
fnielsen <fn@imm.dtu.dk>
2019-07-31 17:25:05 +0200

    Add begali as allowed language

--- ordia/app/views.py
+++ ordia/app/views.py
@@ -14,8 +14,8 @@ from ..text import lowercase_first_sentence_letters, text_to_words
 
 
 ALLOWED_LANGUAGES = [
-    'ar',
-    'da', 'br', 'de', 'en', 'es', 'fr', 'hi', 'it', 'jp', 'ml',
+    'ar', 'bn', 'br', 
+    'da', 'de', 'en', 'es', 'fr', 'hi', 'it', 'jp', 'ml',
     'nl', 'nn', 'no', 'pl', 'pt', 'ru', 'sv', 'vi', 'zh',
 ]
 
